fd25455df6360440d0f70fd4f7dbd1c2d1665837
Piotr Adamczyk <xeno@xeno.yt>
2018-11-24 23:36:30 +0100

    basic auth/app flow in App.js

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
+import CouponHistoryView from "./views/CouponHistoryView";
+import CouponMapView from "./views/CouponMapView";
+import CouponView from "./views/CouponView";
+import LastWinningsView from "./views/LastWinningsView";
+import PendingCouponsView from "./views/PendingCouponsView";
+import SendCouponView from "./views/SendCouponView";
+import AuthLoadingView from "./views/AuthLoadingView";
+import AuthView from "./views/AuthView";
+import AuthRegisterView from "./views/AuthRegisterView";
 
-export default class App extends Component {
-  render() {
-    return <View style={styles.container} />;
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  }
+const AppStack = createStackNavigator({
+  CouponHistory: CouponHistoryView,
+  CouponMap: CouponMapView,
+  Coupon: CouponView,
+  LastWinnings: LastWinningsView,
+  PendingCoupons: PendingCouponsView,
+  SendCoupon: SendCouponView
 });
+const AuthStack = createStackNavigator({
+  SignIn: AuthView,
+  Register: AuthRegisterView
+});
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      AuthLoading: AuthLoadingView,
+      App: AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "AuthLoading"
+    }
+  )
+);
