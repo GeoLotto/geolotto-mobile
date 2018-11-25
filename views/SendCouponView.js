f576ecf536ca9b70de8b3b70a5fdf280ce2d4e7b
Piotr Adamczyk <xeno@xeno.yt>
2018-11-25 06:52:35 +0100

    SendCoupon layout

--- views/SendCouponView.js
+++ views/SendCouponView.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, ActivityIndicator, Text } from "react-native";
+import { Card, Input, Button } from "react-native-elements";
+
+import Ethers from "../utils/Ethers";
+import { ethers } from "ethers";
 
 export default class SendCouponView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wallet: null
+    };
+  }
+  async componentWillMount() {
+    const wallet = await Ethers.getWallet();
+    const balance = await wallet.getBalance();
+    this.setState({ wallet, balance });
+  }
   render() {
-    return <View />;
+    const { wallet, balance } = this.state;
+
+    if (!wallet)
+      return (
+        <View>
+          <ActivityIndicator />
+        </View>
+      );
+    return (
+      <View>
+        <Card title="Adres Ethereum">
+          <Text>{wallet.address}</Text>
+        </Card>
+        <Card title="Ilość ETH w portfelu">
+          <Text>{ethers.utils.formatEther(balance)}</Text>
+        </Card>
+        <Card title="Wartość zakładu">
+          <Input keyboardType={"decimal-pad"} />
+        </Card>
+        <Button
+          style={{
+            width: "93%",
+            marginTop: 20,
+            marginLeft: "auto",
+            marginRight: "auto"
+          }}
+          title="Kup zakład"
+        />
+      </View>
+    );
   }
 }
