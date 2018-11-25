b01e8cd9a5bf52ab1b8641d9aede48aa1043f1f5
Piotr Adamczyk <xeno@xeno.yt>
2018-11-25 02:38:10 +0100

    geolocation shown on map

--- views/CouponMapView.js
+++ views/CouponMapView.js
@@ -5,19 +5,53 @@ import MapView from "react-native-maps";
 import DebugInfo from "../components/DebugInfo";
 
 export default class CouponMapView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coords: {
+        latitude: 0,
+        longitude: 0
+      }
+    };
+  }
+  componentWillMount() {
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({ coords: position.coords });
+      },
+      error => {
+        alert(JSON.stringify(error));
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      }
+    );
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
   render() {
+    alert(JSON.stringify(this.state.coords));
     return (
       <View>
         <MapView
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+            latitude: this.state.coords.latitude,
+            longitude: this.state.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
           }}
+          region={{
+            latitude: this.state.coords.latitude,
+            longitude: this.state.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+          }}
+          showsUserLocation={true}
           style={{ width: "100%", height: "100%" }}
         />
-        <DebugInfo />
       </View>
     );
   }
